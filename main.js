b95de6d15bb8a62f8763ccebc1b58c2f73de373f
Moira Leon <86265787+moiraleon@users.noreply.github.com>
2021-07-22 14:09:23 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+// residentsButton = document.querySelector("#residents-Btn")
+// //document.getElementById("residentsButton").onclick
+// function clicked() {
+//     residentsButton.onclick  
+//     console.log('button clicked') 
+    
+// }
+
+let residentsButton = document.querySelector("#residents-Btn")
+residentsButton.addEventListener('click', () => {console.log('button clicked')})
+
